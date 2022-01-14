e46b26aed2bb8b5d57e3ec1711ce2b62cfa3bdbf
Chris Crouch <chris.crouch2000@gmail.com>
2022-01-14 19:48:22 +0100

    figure plotter updated

--- Python/Modules/Community_Plant/Resilient_SmartCommunity_Plant.py
+++ Python/Modules/Community_Plant/Resilient_SmartCommunity_Plant.py
@@ -466,7 +466,177 @@ class SmartCommunity_Plant:
         
         
     def HEMS_Plant_FigurePlotter(self, HEMS_Plant_FigurePlotter_Input):
+        EToP_Converter= (60/10) # kWh --> W
         
+        ## HEMS_Plant_FigurePlotter - Plotting Figures for Controller and Plant
+
+        ## Getting the desired data from the HEMS_Plant_Baseline_FigurePlotter_Input - Struct
+
+        #----------------HEMS_Plant_Baseline_FigurePlotter_Input------------------#
+        X_k_Plant_History = HEMS_Plant_FigurePlotter_Input['X_k_Plant_History']
+        U_k_History = HEMS_Plant_FigurePlotter_Input['U_k_History']
+        E_LoadData = HEMS_Plant_FigurePlotter_Input['E_LoadData']
+        E_Load_Desired = HEMS_Plant_FigurePlotter_Input['E_Load_Desired']
+        HEMSWeatherData_Output = HEMS_Plant_FigurePlotter_Input['HEMSWeatherData_Output']
+        HEMSPlant_Params = HEMS_Plant_FigurePlotter_Input['HEMSPlant_Params']
+        Community_Params = HEMS_Plant_FigurePlotter_Input['Community_Params']
+        Baseline_Output_Images_Path = HEMS_Plant_FigurePlotter_Input['Baseline_Output_Images_Path']
+        Single_House_Plotting_Index = HEMS_Plant_FigurePlotter_Input['Single_House_Plotting_Index']
+        Simulation_Params = HEMS_Plant_FigurePlotter_Input['Simulation_Params']
+        
+        #-----------------------HEMSWeatherData_Output----------------------------#
+        Ws=HEMSWeatherData_Output['Ws']
+        T_am=HEMSWeatherData_Output['T_am']
+        GHI=HEMSWeatherData_Output['GHI']
+        DNI=HEMSWeatherData_Output['DNI']
+        DateTimeVector=HEMSWeatherData_Output['DateTimeVector']
+        DateTime_Matrix=HEMSWeatherData_Output['DateTime_Matrix']
+
+        #---------------------------HEMSPlant_Params------------------------------#
+
+        E_AC=HEMSPlant_Params['E_AC']
+        T_AC_max=HEMSPlant_Params['T_AC_max']
+        T_AC_min=HEMSPlant_Params['T_AC_min']
+        ACLoad_StartUp_Power=HEMSPlant_Params['ACLoad_StartUp_Power']
+        Eff_Inv=HEMSPlant_Params['Eff_Inv']
+
+        Battery_Energy_Max=HEMSPlant_Params['Battery_Energy_Max']
+        Battery_Energy_Min=HEMSPlant_Params['Battery_Energy_Min']
+        MaxRate_Charging=HEMSPlant_Params['MaxRate_Charging']
+        MaxRate_Discharging=HEMSPlant_Params['MaxRate_Discharging']
+        Eff_Charging_Battery=HEMSPlant_Params['Eff_Charging_Battery']
+        Eff_Discharging_Battery=HEMSPlant_Params['Eff_Discharging_Battery']
+        MaxRate_Discharging_StartUp=HEMSPlant_Params['MaxRate_Discharging_StartUp']
+
+        #---------------------------Community_Params['-----------------------------#
+        N_House=Community_Params['N_House']
+        N_PV_Bat=Community_Params['N_PV_Bat']
+        N_Bat=Community_Params['N_Bat']
+        N_PV=Community_Params['N_PV']
+        N_None=Community_Params['N_None'] 
+
+        #---------------------------Simulation_Params-----------------------------#
+
+        Simulation_StepSize=Simulation_Params['Simulation_StepSize']
+        
+        # Basic Computation and Generating plotable quantities
+
+        # House Numbers
+        N1=N_PV_Bat
+        N2=N_Bat
+        N3=N_PV
+        N4=N_None
+
+        # Truncating Plant History
+        X_k_Plant_History=X_k_Plant_History[:,1:X_k_Plant_History.shape[0],:]
+        
+        # PV Quantities - Individual Houses
+        House_PV_E_Available=X_k_Plant_History[:,:,1]
+        House_PV_E_Used=X_k_Plant_History[:,:,2]
+        House_PV_E_UnUsed=X_k_Plant_History[:,:,3]
+        
+        # Battery Quantities - Individual Houses
+        House_Bat_E_State=X_k_Plant_History[:,:,4]
+        House_Bat_E_Charging=X_k_Plant_History[:,:,5]
+        House_Bat_E_Discharging=X_k_Plant_History[:,:,6]
+        
+        # House Temperature Quantities - Individual Houses
+        House_Temprature=X_k_Plant_History[:,:,7]
+        
+        # House Energy Quantities - Individual Houses
+        House_Bat_E_OtherLoad_Desired=E_Load_Desired[:,:,1]
+        House_Bat_E_ACLoad_Desired=U_k_History[:,:,3] * [E_AC/Eff_Inv]  #***
+        House_Bat_E_TotalLoad_Desired=House_Bat_E_OtherLoad_Desired + House_Bat_E_ACLoad_Desired
+        
+        House_Bat_E_OtherLoad_Actual=X_k_Plant_History[:,:,12]
+        House_Bat_E_ACLoad_Actual=X_k_Plant_History[:,:,11]
+        House_Bat_E_TotalLoad_Actual=House_Bat_E_OtherLoad_Actual+House_Bat_E_ACLoad_Actual
+
+        # House Controller Quantities - Individual Houses
+        House_Bat_Controller_Charging_Desired=U_k_History[:,:,1]
+        House_Bat_Controller_Discharging_Desired=U_k_History[:,:,2]
+
+        House_AC_Controller_TurnOn_Desired=U_k_History[:,:,3]
+        House_AC_Controller_TurnOn_Actual=X_k_Plant_History[:,:,21]
+
+#sum functions is different in matlab need to change it to python
+        # PV Quantities - All Houses [Addup]
+        Community_PV_E_Available=sum(X_k_Plant_History[:,:,1],0)
+        Community_PV_E_Used=sum(X_k_Plant_History[:,:,2],0)
+        Community_PV_E_UnUsed=sum(X_k_Plant_History[:,:,3],0)
+
+        # Battery Quantities - Individual Houses - All Houses [Addup]
+        Community_Bat_E_State=sum(X_k_Plant_History[:,:,4],0)
+        Community_Bat_E_Charging=sum(X_k_Plant_History[:,:,5],0)
+        Community_Bat_E_Discharging=sum(X_k_Plant_History[:,:,6],0)
+
+        # House Temperature Quantities - Individual Houses - All Houses [Average]
+        Community_Temprature=mean[X_k_Plant_History[:,:,7],0]
+
+        # House Energy Quantities - Individual Houses - All Houses [Addup]
+        Community_Bat_E_OtherLoad_Desired=sum(E_Load_Desired[:,:,1],0)
+        Community_Bat_E_ACLoad_Desired=sum(House_Bat_E_ACLoad_Desired[:,:,1],0)
+        Community_Bat_E_TotalLoad_Desired=Community_Bat_E_ACLoad_Desired+Community_Bat_E_OtherLoad_Desired
+
+        Community_Bat_E_OtherLoad_Actual=sum(X_k_Plant_History[:,:,12],0)
+        Community_Bat_E_ACLoad_Actual=sum(X_k_Plant_History[:,:,11],0)
+        Community_Bat_E_TotalLoad_Actual=Community_Bat_E_OtherLoad_Actual+Community_Bat_E_ACLoad_Actual
+
+        # House Controller Quantities - Individual Houses - All Houses [Addup]
+        Community_Bat_Controller_Charging_Desired=sum(U_k_History[:,:,1],0)
+        Community_Bat_Controller_Discharging_Desired=sum(U_k_History[:,:,2],0)
+
+        Community_AC_Controller_TurnOn_Desired=sum(U_k_History[:,:,3],0)
+        Community_AC_Controller_TurnOn_Actual=sum(X_k_Plant_History[:,:,21],0)
+        
+        # Computing House and Community Battery SoC
+        House_Bat_SoC=(((House_Bat_E_State)-Battery_Energy_Min)/(Battery_Energy_Max-Battery_Energy_Min))*100
+        Community_Bat_SoC=(((Community_Bat_E_State)-((N1+N2)*Battery_Energy_Min))/(((N1+N2)*Battery_Energy_Max)-((N1+N2)*Battery_Energy_Min)))*100
+        
+        # Computing House and Community Generation and Demand
+        House_E_Generation=House_PV_E_Used+House_Bat_E_Discharging
+        House_E_Demand=House_Bat_E_Charging+House_Bat_E_TotalLoad_Actual
+        
+        Community_E_Generation=Community_PV_E_Used+Community_Bat_E_Discharging
+        Community_E_Demand=Community_Bat_E_Charging+Community_Bat_E_TotalLoad_Actual
+        
+#had to make arrays ones instead of zeros because of numpy
+        #House/Community Level Battery Charging_Dispatchable/Discharging_Dispatchable
+        House_Bat_E_Charging_Dispatchable=np.ones((N_House,len(GHI),1)) # Initialization
+        House_Bat_E_Discharging_Dispatchable=np.ones((N_House,length(GHI),1)) # Initialization
+        
+        for i in range(len(GHI)-1):
+            if (Community_PV_E_Available(1,i,1) >= Community_Bat_E_TotalLoad_Desired(1,ii,1)):
+                for j in  range(N_PV_Bat+N_Bat-1):
+                    House_Bat_E_Charging_Dispatchable(j,i,1)=min(MaxRate_Charging*Simulation_StepSize, (Battery_Energy_Max-(X_k_Plant_History(j,i,4))) / Eff_Charging_Battery)
+                    House_Bat_E_Discharging_Dispatchable(j,i,1)=0;
+            else:
+                for j in range(N_PV_Bat+N_Bat-1):
+                    House_Bat_E_Charging_Dispatchable(j,i,1)=0
+                    House_Bat_E_Discharging_Dispatchable(j,i,1)=min(MaxRate_Discharging*Simulation_StepSize,(X_k_Plant_History(j,i,4)-Battery_Energy_Min)*Eff_Discharging_Battery)
+        
+        Community_Bat_E_Charging_Dispatchable=sum(House_Bat_E_Charging_Dispatchable[:,:,1],0)#***
+        Community_Bat_E_Discharging_Dispatchable=sum(House_Bat_E_Discharging_Dispatchable[:,:,1],0)#***
+
+        # House AC Startup Power Quantities
+        Community_AC_P_StartUp_Available=(Community_PV_E_Available/Simulation_StepSize)+(Community_Bat_Controller_Charging_Desired*MaxRate_Discharging_StartUp)
+
+        House_AC_StartUp_Desired=House_AC_Controller_TurnOn_Desired[:,:,1]-X_k_Plant_History[:,:,30]
+        House_AC_StartUp_Desired = (abs(House_AC_StartUp_Desired)+House_AC_StartUp_Desired)/2
+        Community_AC_StartUp_Desired = sum(House_AC_StartUp_Desired[:,:,1],0)
+        Community_AC_P_StartUp_Required=Community_AC_StartUp_Desired*ACLoad_StartUp_Power
+
+        House_AC_StartUp_Actual=X_k_Plant_History[:,:,21]-X_k_Plant_History[:,:,30]
+        House_AC_StartUp_Actual = (abs(House_AC_StartUp_Actual)+House_AC_StartUp_Actual)/2
+        Community_AC_StartUp_Actual = sum(House_AC_StartUp_Actual[:,:,1],0)
+        Community_AC_P_StartUp_Used=Community_AC_StartUp_Actual*ACLoad_StartUp_Power
+
+        # Creating Grouping Indices
+        N_All_Indices = range(1,N_House)
+        N_PV_Bat_Only_Indices = range(1,N1)
+        N_Bat_Only_Indices = range(N1+1,N2)
+        N_PV_Only_Indices = range(N2+1,N3)
+        N_None_Only_Indices = range(N3+1,N4)
         
         
         # To be Continued.........................................................     
